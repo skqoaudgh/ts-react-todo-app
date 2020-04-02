a121616902f6fc000feae79d52d5bc8f05ba7e5c
myungho <skqoaudgh2@hanmail.net>
2020-04-02 15:28:26 +0900

    Todo 리스트 아이템 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TodoListItem } from './TodoListItem';
+
+const initialTodos: Array<Todo> = [
+  {text: "Walk the dog", complete: true}, 
+  {text: "Write app", complete: false}
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if(todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
+
   return (
-    <div>Hello World!</div>
+    <React.Fragment>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
+    </React.Fragment>
   );
 }
 
